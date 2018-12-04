3650af517e9742542337ce41ae83e193bea86a07
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2018-12-04 23:40:28 +0530

    add mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -9,23 +9,39 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
- console.log('newMessage', message);
  var formattedTime = moment(message.createdAt).format('h:mm a')
- var li = jQuery('<li></li>');
- li.text(`${message.from}: ${formattedTime} : ${message.text}`);
+ var template = jQuery('#message-template').html();
+ var html = Mustache.render(template, {
+     text:message.text,
+     from: message.from,
+     createdAt:formattedTime
+ })
+   jQuery('#messages').append(html);
+//  var li = jQuery('<li></li>');
+//  li.text(`${message.from}: ${formattedTime} : ${message.text}`);
 
- jQuery('#messages').append(li);
+//  jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target = "_blank">My Location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a')
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+     from:message.from,
+     url:message.url,
+     createdAt:formattedTime
+ })
+   jQuery('#messages').append(html);
 
-    li.text(`${message.from}: `);
-    a.attr('href', message.url)
 
-    li.append(a);
-    jQuery('#messages').append(li);
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target = "_blank">My Location</a>');
+
+    // li.text(`${message.from}: `);
+    // a.attr('href', message.url)
+
+    // li.append(a);
+    // jQuery('#messages').append(li);
 
 
 })
@@ -33,12 +49,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
    e.preventDefault();
-
+   var messageTextBox = jQuery('[name=message]');
    socket.emit('createMessage', {
        from:'User',
-       text: jQuery('[name=message]').val()
+       text:messageTextBox.val()
    }, function () {
-
+       messageTextBox.val('')
    })
 })
 
