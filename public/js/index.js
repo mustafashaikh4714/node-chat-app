5fd9108dfa37d0bd54761d06527788a4e5e70168
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2018-12-04 22:39:19 +0530

    add geolocation with geolocation api and format timestamp using moment

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,25 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
  console.log('newMessage', message);
+ var formattedTime = moment(message.createdAt).format('h:mm a')
  var li = jQuery('<li></li>');
- li.text(`${message.from}: ${message.text}`);
+ li.text(`${message.from}: ${formattedTime} : ${message.text}`);
 
  jQuery('#messages').append(li);
 });
+
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target = "_blank">My Location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url)
+
+    li.append(a);
+    jQuery('#messages').append(li);
+
+
+})
 
 
 jQuery('#message-form').on('submit', function (e) {
@@ -26,4 +40,23 @@ jQuery('#message-form').on('submit', function (e) {
    }, function () {
 
    })
+})
+
+var buttonLocation = jQuery('#send-location');
+
+buttonLocation.on('click', function () {
+    if(!navigator.geolocation) {
+       return alert('geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage',  {
+        latitude: position.coords.latitude, 
+        longitude:position.coords.longitude
+    })
+    
+    }, function () {
+        console.log('unable to fetch the location');
+        
+    })
 })
