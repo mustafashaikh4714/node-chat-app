46b7b9f7f5c4c7d975af2cbcebe368b18ca464dc
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2018-12-04 17:12:32 +0530

    add form to chat app

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const publicPath = path.join(__dirname, '../public');
 const {generateMessage} = require('./utils/message')
+const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-var port = process.env.PORT || 3000
 app.use(express.static(publicPath));
+var port = process.env.PORT || 3000
 
  io.on('connection', (socket) => {
     console.log('New user is Connected');
@@ -18,10 +18,11 @@ app.use(express.static(publicPath));
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to Chat App'))
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-    socket.on('createMessage', function (message) {
+    socket.on('createMessage', (message, callback) => {
        console.log('createMessage', message);
-       io.emit('newMessage', generateMessage(message.from, message.text))
-    })
+       io.emit('newMessage', generateMessage(message.from, message.text));
+       callback();
+    });
 
     socket.on('disconnect', () => {
         console.log('user was disconnected');  
